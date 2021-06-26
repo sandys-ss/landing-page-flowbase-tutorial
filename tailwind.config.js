7ada8a062d7dfe7d684af40ac8c5ded9a681a43f
sandys-ss <sandysyam@gmail.com>
2021-06-26 17:58:58 +0800

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         head: ['"DM Sans"'],
         body: ['Mulish']
       },
+
       colors: {
         transparent: 'transparent',
         black: '#19191B',
@@ -14,6 +15,33 @@ module.exports = {
         blue: '#5454D4',
         orange: '#FF7143',
         grey: '#696871'
+      },
+
+      spacing: {
+        mtop: '30px',
+        mgrid: '185px',
+        navheight: '60px',
+        mbutton: '19px',
+        pbottom: '10px'
+      },
+
+      fontSize: {
+        freguler: ['15px', {
+          letterSpacing: '-0.47px',
+          lineHeight: '20px',
+        }],
+        fbutton: ['17px', {
+          letterSpacing: '-0.53px',
+          lineHeight: '22px',
+        }],
+      },
+
+      gap: {
+        ggrid: '30px'
+      },
+
+      borderRadius: {
+        large: '10px',
       }
     },
   },
